d3873381e8b324ecabfc3d24ecaa1ef733e51564
tecosama <taylancankose@gmail.com>
2022-05-10 13:52:10 +0300

    typing-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from "react-redux";
+import "./App.css";
+import Result from "./components/Result";
+import Timer from "./components/Timer";
+import UserInput from "./components/UserInput";
+import Words from "./components/Words";
 
 function App() {
+  const status = useSelector((state) => state.typing.status);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="word-section">
+        <Words />
+      </div>
+      <div className="timer-section">
+        <Timer />
+      </div>
+      <div className="input-section">
+        <UserInput />
+      </div>
+      {status === "finished" && (
+        <div className="result-section">
+          {status === "finished" && <Result />}
+        </div>
+      )}
     </div>
   );
 }
